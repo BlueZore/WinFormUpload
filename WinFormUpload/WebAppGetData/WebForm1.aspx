a1d0421d94182c811e7a806779873cb45138cb91
BlueZore <kally32@163.com>
2014-11-14 10:29:28 +0800

    1

---
+++ WinFormUpload/WebAppGetData/WebForm1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" ValidateRequest="false" %>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" >
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta name="GENERATOR" content="Microsoft Visual Studio .NET 7.1" />
+    <meta name="CODE_LANGUAGE" content="C#" />
+    <meta name="vs_defaultClientScript" content="JavaScript" />
+    <meta name="vs_targetSchema" content="http://schemas.microsoft.com/intellisense/ie5" />
+    <title></title>
+</head>
+<body>
+      <form id="Form1" method="post" runat="server">
+  </form>
+</body>
+</html>
---
+++ WinFormUpload/WinFormUpload/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.DoWork+=bw_DoWork;
+            bw.RunWorkerAsync();
+            
+        }
+
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            WebClient myWebClient = new WebClient();
+            myWebClient.UploadFile("http://172.31.132.44:8891/WebForm1.aspx", "POST", @"D:/soft/ietester.zip");
+            label1.Invoke(new Action(() => label1.Text = "ok"));
+        }
+    }
+}
