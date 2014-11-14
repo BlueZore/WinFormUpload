04f8a707b77a7c812b850e03970ac54a37d1c169
BlueZore <kally32@163.com>
2014-11-14 13:20:43 +0800

    2

--- WinFormUpload/WinFormUpload/Form1.Designer.cs
+++ WinFormUpload/WinFormUpload/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApplication1
+﻿namespace WinFormUpload
 {
     partial class Form1
     {
@@ -29,22 +29,73 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 9);
+            this.label1.Location = new System.Drawing.Point(23, 208);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "上传文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(25, 68);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(228, 21);
+            this.txtID.TabIndex = 3;
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(25, 95);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(228, 21);
+            this.txtType.TabIndex = 4;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(25, 122);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(228, 21);
+            this.txtPath.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +107,11 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.TextBox txtPath;
     }
 }
 
--- WinFormUpload/WinFormUpload/Form1.cs
+++ WinFormUpload/WinFormUpload/Form1.cs
@@ -10,26 +10,63 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication1
+namespace WinFormUpload
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-            
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
             BackgroundWorker bw = new BackgroundWorker();
-            bw.DoWork+=bw_DoWork;
+            bw.DoWork += bw_DoWork;
             bw.RunWorkerAsync();
-            
         }
 
         private void bw_DoWork(object sender, DoWorkEventArgs e)
         {
             WebClient myWebClient = new WebClient();
-            myWebClient.UploadFile("http://172.31.132.44:8891/WebForm1.aspx", "POST", @"D:/soft/ietester.zip");
+            myWebClient.UploadFile("http://172.31.132.44:8892/WebForm1.aspx", "POST", @"D:/soft/ietester.zip");
             label1.Invoke(new Action(() => label1.Text = "ok"));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.DoWork += bw_DoWork2;
+            bw.RunWorkerAsync();
+        }
+
+        private void bw_DoWork2(object sender, DoWorkEventArgs e)
+        {
+            WebClient WClient = new WebClient();
+            System.Collections.Specialized.NameValueCollection VarPost = new System.Collections.Specialized.NameValueCollection();//用于存放post数据
+            VarPost.Add("ID", txtID.Text); //数据ID
+            VarPost.Add("Type", txtType.Text); //标识
+
+            byte[] imageSource = SetImageToByteArray(txtPath.Text);
+            //转Base64
+            string Base64 = Convert.ToBase64String(imageSource);
+            VarPost.Add("ImageID", Guid.NewGuid().ToString()); //图片ID
+            VarPost.Add("Image", Base64); //图片字符
+
+            byte[] RemoteByte = WClient.UploadValues("http://172.31.132.44:8892/WebForm2.aspx", "POST", VarPost);//提交post请求
+            label1.Invoke(new Action(() => label1.Text = "ok"));
+        }
+
+        //根据文件名(完全路径)
+        public byte[] SetImageToByteArray(string fileName)
+        {
+            //图片转二进制
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            int streamLength = (int)fs.Length;
+            byte[] image = new byte[streamLength];
+            fs.Read(image, 0, streamLength);
+            fs.Close();
+            return image;
         }
     }
 }
