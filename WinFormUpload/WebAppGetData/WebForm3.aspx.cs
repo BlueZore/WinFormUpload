312186eeefef22dfce58c2e5b420c08ba4587720
BlueZore <kally32@163.com>
2014-11-15 11:05:48 +0800

    3

---
+++ WinFormUpload/WebAppGetData/WebForm3.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace WebApplication1
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 2015; i < 2019; i++)
+            {
+                for (int j = 1; j < 13; j++)
+                {
+                    string fileName = i.ToString() + j.ToString().PadLeft(2, '0');
+                    Directory.CreateDirectory(Server.MapPath("~/Upload/") + fileName);
+                }
+            }
+        }
+    }
+}
